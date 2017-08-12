57dbf319d1d75ebc9ebf6956154dad156cb2c5d6
infinitepure <matt.j.cheah@gmail.com>
2017-08-12 20:52:45 +1000

    Implement calculation functionality

--- main.js
+++ main.js
@@ -16,27 +16,66 @@ $(document).ready(function() {
         eight: "8",
         nine: "9"
     }
+    var currSymbol = "";
+    var expression = [];
+    var operations = {
+        divi: function(a, b) {return a/b;},
+        mult: function(a, b) {return a*b;},
+        add: function(a, b) {return a+b;},
+        subt: function (a, b) {return a-b;}
+    };
+
     function showSymbol(symbol) {
         $(".symbol p").html(symbol);
     }
-    var currSymbol = ""
+
+    function calculate(expression) {
+        var operands = expression.filter(function(item) {
+            return !operations.hasOwnProperty(item);
+        })
+            .map(function(item) {
+                return parseInt(item);
+            });
+        var ops = expression.filter(function(item) {
+            return operations.hasOwnProperty(item);
+        });
+        var initial = operands.shift();
+        return operands.reduce(function(init, op) {
+            var result = operations[ops[0]](init, op);
+            ops.shift();
+            return result;
+        }, initial);
+    }
+
     $(".btn").click(function(event) {
         var id = event.currentTarget.id;
         if (id === "ac" || id === "ce") {
             currSymbol = "";
+            if (id === "ac") {
+                expression = [];
+            }
             showSymbol("0");
+        }
+        else if (id === "equals") {
+            expression.push(currSymbol);
+            currSymbol = calculate(expression);
+            expression = [];
+            showSymbol(currSymbol);
         }
         else {
             if (currSymbol === strings["mult"] || currSymbol === strings["divi"] || currSymbol === strings["subt"] || currSymbol === strings["add"]) {
                 currSymbol = "";
             }
-            else if (id === "mult" || id === "divi" || id === "subt" || id === "add") {
+            else if (operations.hasOwnProperty(id)) {
+                expression.push(currSymbol);
                 currSymbol = "";
+                expression.push(id);
             }
-            
             currSymbol += strings[id];
             if (currSymbol.length > 7) {
-                currSymbol = "ERROR";
+                alert(currSymbol);
+                expression = ["DIGIT LIMIT MET"];
+                currSymbol = "  ERROR";
             }
             showSymbol(currSymbol);
         }
